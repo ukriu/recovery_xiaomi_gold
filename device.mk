c9b3fcfbd923c83e1c168bbb64494dfd9a600ed5
namakunegro <namakunegro@proton.me>
2025-10-31 13:14:46 +0800

    gold: remove mediatek's boot control library
    Signed-off-by: namakunegro <namakunegro@proton.me>

--- device.mk
+++ device.mk
@@ -110,19 +110,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Bootctrl
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl
-
-TARGET_RECOVERY_DEVICE_MODULES += \
-  android.hardware.boot@1.0.xml \
-  android.hardware.boot@1.1.xml \
-  android.hardware.boot@1.2.xml
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
